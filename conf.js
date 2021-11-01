b1dd92f0390866e8749e3d0ec681a2e3c5fd6a16
LoicBrassart <loic.brassart@gmail.com>
2021-11-01 15:49:20 +0100

    Add general-purpose conf file

---
+++ conf.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_SCHEMA,
+  DB_PASSWORD,
+  API_KEY_BOARDATLAS,
+  /* BACK_PORT,
+  JWT_SALTROUNDS,
+  JWT_SECRET,
+  FRONTEND_URL,
+  ADMIN_URL, */
+} = process.env;
+
+const db = mysql.createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_SCHEMA,
+});
+
+module.exports = {
+  db,
+  apiBoardAtlas: API_KEY_BOARDATLAS,
+};
